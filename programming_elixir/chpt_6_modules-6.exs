5fd02ab905c9206498ebbb4987624ce9bb4369c5
Chae <chaemobile@gmail.com>
2015-07-20 18:21:54 -0400

    Adds recursion and logic for guesing answer

--- programming_elixir/chpt_6_modules-6.exs
+++ programming_elixir/chpt_6_modules-6.exs
@@ -15,23 +15,26 @@
 # Is it 273
 # 273
 
-
 ##########################
-# Psudocode
-# given range = 1..10, guess = 2
-# range 1-10
-# even_range_top 10/2 => midpoint
-# if 5 == 2? => return true
-# 5 < 2  => false
-# odd_range_top 5+1/2 => 3
-# 3 < 2 => false
-# odd_range_top 3+1/2 => 2
-# done
+# Test
+# answer(33, 5, 100, 0) when 33 > 5, do: div(abs(33-100)), 5, 33, 0
+# answer(16, 5, 33, 0) when 16 > 5, do: div(abs(16-33)), 5, 16, 0
+# answer(8, 5, 16, 0) when 8 > 5, do: div(abs(8-16)), 5, 8, 0
+# answer(4, 5, 16, 0) when 4 < 5, do: div(16-4,2), 5, 16, 4
+# answer(6, 5, 16, 0) when 6 > 5, do: div(abs(6-16)), 5, 6, 0
+##########################
 
 defmodule BinarySearch do
+  def answer(guess, actual, upper, lower) when guess == actual, do:
+    IO.puts "#{guess}"
 
-  def answer?(guess, actual) when guess == actual, do: show_answer(actual)
-  def show_answer(answer), do: IO.puts "#{answer}"
+  def answer(guess, actual, upper, lower) when guess > actual, do:
+    answer(div(abs(guess-upper),2), actual, guess, lower)
+
+  def answer(guess, actual, upper, lower) when guess < actual, do:
+    answer(guess + div(abs(upper-guess),2), actual, upper, guess)
 end
 
-BinarySearch.answer?(2, 2)
+BinarySearch.answer(500, 273, 1000, 1)
+BinarySearch.answer(500, 14, 1000, 1)
+BinarySearch.answer(500, 998, 1000, 1)
