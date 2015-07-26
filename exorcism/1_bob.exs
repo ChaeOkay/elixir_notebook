2d373b66df4d9af5a6316293c06a31da584bc7a8
Chae <chaemobile@gmail.com>
2015-07-25 22:13:28 -0400

    First exorcism challenge, passing 3 specs

---
+++ exorcism/1_bob.exs
@@ -0,0 +1,32 @@
+defmodule Tone do
+  def question?(phrase), do: String.contains?(phrase, "?")
+
+  def shouting?(words) when is_list(words), do: _shouting?(words, false)
+  defp _shouting?([], _), do: false
+  defp _shouting?([head | tail], response) when response == true, do: true
+  defp _shouting?([head | tail], response), do: _shouting?(tail, upcase?(head))
+  defp upcase?(word), do: String.upcase(word) == word
+end
+
+defmodule Teenager do
+  import Tone, only: [question?: 1, shouting?: 1]
+  @response %{
+               default: "Whatever.",
+               question: "Sure.",
+               shouting: "Whoa, chill out!",
+              }
+
+  def hey(phrase) do
+    words = String.split(phrase)
+
+    cond do
+      shouting?(words) == true ->
+        @response.shouting
+      question?(phrase) == true ->
+        @response.question
+      true ->
+        @response.default
+    end
+  end
+end
+
